3ecd1ae1a881ad523f8cbd67c4aa59969d5829cd
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-11-14 05:38:47 -0300

    First commit - create spring boot with postgre dependency

---
+++ src/main/java/br/com/solucitiva/apisecurity/SolucitivaApiSecutirtyApplication.java
@@ -0,0 +1,13 @@
+package br.com.solucitiva.apisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucitivaApiSecutirtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolucitivaApiSecutirtyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solucitiva/apisecurity/SolucitivaApiSecutirtyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solucitiva.apisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucitivaApiSecutirtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
